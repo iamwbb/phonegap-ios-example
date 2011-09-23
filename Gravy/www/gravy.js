168d4d4c8ad8c3c28bf69b725d70fdbe25599bcc
Andrew Glover <ajglover@gmail.com>
2011-09-23 19:41:16 -0400

    updated documentation

--- Gravy/www/gravy.js
+++ Gravy/www/gravy.js
@@ -5,8 +5,11 @@
  $(document).ready(function(){ $(document).bind('deviceready', function(){ onDeviceReady() })});   
  
  function onDeviceReady(){
+    //log call, in this case of type info. Other choices include debug, warn, and error.
     window.plugins.app47.info(["app started up"]);
- 
+    //runtime configuration, in this case in group PG with a key of endpoint_2
+    //the value returned is a string (URL). The use case here is that various 
+    //values can be updated/changed without redeploying new instances of an App
     window.plugins.app47.getValue("PG", "endpoint_2", 
         function(result){
           enpointURL = result;
@@ -26,13 +29,15 @@
         tgs.blur();
         
         var timedEventID = 0;
-
+        //starting a timed event -- this returns an ID which is then 
+        //used to mark the completion of the event
         window.plugins.app47.startTimedEvent(["deal submission"],  
                     function(result){  timedEventID = result; });
                              
         $.ajax({
               type: "PUT",
-              url: enpointURL,
+              //endpointURL was obtained via App47 SDK configuration value above 
+              url: enpointURL, 
               data: JSON.stringify({ deal_description: descp.val() , all_tags: tgs.val() }),
               contentType: 'application/json', 
               dataType: 'json', 
@@ -44,7 +49,9 @@
                     $("#notice").fadeOut(3200);
                      
                 });
+                //generic events have no notion of time -- they are similar to log stmts
                 window.plugins.app47.sendGenericEvent(["deal submitted"]);                        
+                //end the timed event started above
                 window.plugins.app47.endTimedEvent([timedEventID]); 
               }
         });
