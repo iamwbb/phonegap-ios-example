9df31e58be351beaeb8577dc083ea1a0f9734a7d
Andrew Glover <ajglover@gmail.com>
2011-09-23 12:58:29 -0400

    updated documentation of plugin js file

--- Gravy/www/app47pg.js
+++ Gravy/www/app47pg.js
@@ -1,3 +1,18 @@
+/**
+  
+ App47 Plugin for PhoneGap.
+   Created by the team @ App47, Inc.
+
+  1. Include the App47 SDK files in Xcode (follow instructions from App47 documentation)
+  2. Put App47PGPlugin.h and App47PGPlugin.m into PhoneGap Plugins/ directory 
+  3. Add app47pg.js into the www/ folder, and reference it in a script tag *after* phonegap.js
+  4. In PhoneGap.plist (found in Supporting Files/ folder), under the 'Plugins' key, add "com.app47.sdk" w/value "App47PGPlugin"
+
+  The plugin's JavaScript functions are under the global "window.plugins.app47" object. 
+  See <sample project> for Plugin usage in sample iOS project.
+ 
+ */
+
 if (!PhoneGap.hasResource("App47")) {
 	PhoneGap.addResource("App47");
 
